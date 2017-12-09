4a9741d3776ebc86d2ee3d4d3dfcefe1fc704366
pedro.pinheiro <pedro.hendrixx@gmail.com>
2017-12-09 17:57:21 -0200

    alteracao classe pessoa

--- src/main/java/com/renostarter/model/Pessoa.java
+++ src/main/java/com/renostarter/model/Pessoa.java
@@ -2,4 +2,16 @@ package com.renostarter.model;
 
 public abstract class Pessoa {
 
+	protected Contato contato;
+	protected Endereco endereco;
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
 }
