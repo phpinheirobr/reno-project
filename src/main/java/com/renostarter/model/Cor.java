81a93ba8bc27d9c66789b09d42aea9124073be50
pedro.pinheiro <pedro.hendrixx@gmail.com>
2018-01-20 19:20:25 -0200

    [CREATE] criação das entidades veiculo e cor

---
+++ src/main/java/com/renostarter/model/Cor.java
@@ -0,0 +1,27 @@
+package com.renostarter.model;
+
+public class Cor {
+	
+	private Integer id;
+	
+	private String cor;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/renostarter/model/Veiculo.java
@@ -0,0 +1,80 @@
+package com.renostarter.model;
+
+import java.sql.Date;
+
+public class Veiculo {
+
+	private Integer id;
+	private String nome;
+	private String placa;
+	private Integer qtdPorta;
+	private Integer quilometragem;
+	private Cor cor;
+	private Date anoFabricacao;
+	private Marca marca;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public Integer getQtdPorta() {
+		return qtdPorta;
+	}
+
+	public void setQtdPorta(Integer qtdPorta) {
+		this.qtdPorta = qtdPorta;
+	}
+
+	public Integer getQuilometragem() {
+		return quilometragem;
+	}
+
+	public void setQuilometragem(Integer quilometragem) {
+		this.quilometragem = quilometragem;
+	}
+
+	public Cor getCor() {
+		return cor;
+	}
+
+	public void setCor(Cor cor) {
+		this.cor = cor;
+	}
+
+	public Date getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+	public void setAnoFabricacao(Date anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+}
